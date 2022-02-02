c17f3cbc6da3508f05dc4503331dca682b0b3600
Harvey Schneider <50023408+shimthal61@users.noreply.github.com>
2022-02-02 20:48:24 +0000

    Delete Bee Colony losses directory

--- Bee Colony losses/Bee Colony losses.R
+++
@@ -1,8 +0,0 @@
-library(tidyverse)
-library(tidytuesdayR)
-
-colony <- read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-01-11/colony.csv")
-stressor <- read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-01-11/stressor.csv")
-
-head(colony)
-head(stressor)
